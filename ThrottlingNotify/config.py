a426893278ad646b41c1983d29a629f2e984c343
flabbe <flabbe@coveo.com>
2016-12-15 16:11:37 -0500

    Slack client util

--- ThrottlingNotify/config.py
+++ ThrottlingNotify/config.py
@@ -2,7 +2,16 @@ THROTTLING_BUCKET_NAME = 'hackathon-throttling-events'
 FETCH_FREQUENCY_MS = 5000
 
 NOTIFY_LEVELS = {
-    1: 'Alert level 1',
-    2: 'Alert level 2',
-    3: 'Alert level 3'
+    1: {
+        'alert_title': 'Alert level 1',
+        'alert_message_fmt': 'Organization {} is a dick'
+    },
+    2: {
+        'alert_title': 'Alert level 2',
+        'alert_message_fmt': 'Organization {} is a super dick'
+    },
+    3: {
+        'alert_title': 'Alert level 3',
+        'alert_message_fmt': 'Organization {} is not worth it'
+    }
 }
