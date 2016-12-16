aa1993f27ea59e6541b7db278e466da0019d73ea
Emile Fugulin <emilefugulin@hotmail.com>
2016-12-16 09:45:50 -0500

    Add email notifications

---
+++ Utils/email_notifier.py
@@ -0,0 +1,31 @@
+from smtplib import SMTP
+from ConfigParser import ConfigParser
+from email.mime.multipart import MIMEMultipart
+from email.utils import formatdate
+from email.mime.text import MIMEText
+
+
+class EmailNotifier:
+    def __init__(self, config_file):
+        config = ConfigParser()
+        config.read(config_file)
+        self.server = config.get('smtp', 'server')
+        self.port = config.get('smtp', 'port')
+        self.username = config.get('smtp', 'username')
+        self.password = config.get('smtp', 'password')
+        self.sender = config.get('smtp', 'sender')
+
+    def send_email(self, receiver, subject, text):
+        message = MIMEMultipart('related')
+        message['From'] = self.sender
+        message['To'] = receiver
+        message['Date'] = formatdate(localtime=True)
+        message['Subject'] = subject
+
+        message.attach(MIMEText(text))
+
+        smtp = SMTP(self.server, int(self.port))
+        smtp.starttls()
+        smtp.login(self.username, self.password)
+        smtp.sendmail(self.sender, receiver, message.as_string())
+        smtp.close()
